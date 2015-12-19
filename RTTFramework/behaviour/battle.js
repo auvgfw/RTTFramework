e9aaebfb35f72495e070270c56294d538d764318
ZouLance <double_again@hotmail.com>
2015-12-19 12:29:43 +0800

    init with visual exist files

---
+++ RTTFramework/behaviour/battle.js
@@ -0,0 +1,43 @@
+﻿var db_config=
+ {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'armygirl',
+};
+var mysql = require('mysql');
+
+/*
+function generatedata()
+ {
+
+    console.log('datagenerator');
+    var sql = 'select * from user_info where user_id=' + player1id;
+    connection.query
+        (
+        sql, function selectusers(err, results, fields) {
+            if (err) {
+                throw err;
+            }
+            for (var i = 1; i <= 6; i++) {
+                //console.log('query finish');
+                player1Group[i]['instance_id'] = results[0]['position_' + i + '_instance_id'];
+                //data += 'instance_id: ' + player1Group[i]['instance_id']+'<br>';
+                data += results[0]['position_' + i + '_instance_id'] + '<br>';
+                console.log(data);
+            }
+        } 
+    );
+
+     return "it is data";
+}
+*/
+
+
+
+var Battle = function (player1id, player2id) {
+    this.generatedata = function () {
+        return "it is data";
+    };
+};
+module.exports = Battle;
---
+++ RTTFramework/routes/battle.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var router = express.Router();
+var Battle = require('../behaviour/battle');
+/* GET users listing. */
+router.get('/', function (req, res) {
+        res.send('this is battle index');
+    }
+);
+
+router.get('/:id1/:id2/record', function (req, res){
+    var id1 = req.params.id1;
+    var id2 = req.params.id2;
+    var battle = new Battle(id1,id2);
+    var send_data = battle.generatedata();
+    res.send(req.params.id1 + req.params.id2+send_data);
+    }
+)
+
+module.exports = router;
